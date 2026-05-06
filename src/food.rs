d7c13fb33b30f816e96d369294245ebaf15f629e
newbiners <gufronnewbiners@gmail.com>
2026-05-06 10:16:16 +0700

    add scale food

--- src/food.rs
+++ src/food.rs
@@ -6,21 +6,26 @@ pub struct Food {
 
 impl Food {
     pub fn new() -> Self {
-        Self {
-            position: (15, 15),
-        }
+        Self { position: (15, 15) }
     }
 
     pub fn respawn(&mut self) {
-        self.position = (rand::random::<i32>().abs() % 20, rand::random::<i32>().abs() % 20);
+        self.position = (
+            rand::random::<i32>().abs() % 20,
+            rand::random::<i32>().abs() % 20,
+        );
     }
 
-    pub fn draw(&self, canvas: &mut ggez::graphics::Canvas, assets: &crate::assets::Assets) -> GameResult {
-        use ggez::graphics::DrawParam;
+    pub fn draw(
+        &self,
+        canvas: &mut ggez::graphics::Canvas,
+        assets: &crate::assets::Assets,
+    ) -> GameResult {
         use crate::utils::grid_to_pixel;
+        use ggez::graphics::DrawParam;
 
         let pos = grid_to_pixel(self.position);
-        canvas.draw(&assets.apple, DrawParam::default().dest([pos.0, pos.1]).scale([0.5, 0.5]));
+        canvas.draw(&assets.apple, DrawParam::default().dest([pos.0, pos.1]));
         Ok(())
     }
 }
